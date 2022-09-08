8a9645267b82855749198fe3ecea60e13d2dd0a2
Tim254 <tim.mailu@gmail.com>
2022-09-09 00:26:45 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import './App.css';
+import { Routes, BrowserRouter, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import AddEditBlog from './pages/AddEditBlog';
+import About from './pages/About';
+import NotFound from './pages/NotFound';
+import Blog from './pages/Blog';
+import { ToastContainer} from 'react-toastify'
+import "react-toastify/dist/ReactToastify.css";
+
+function App() {
+  return (
+    <BrowserRouter>
+       <div className="App">
+        <ToastContainer/>
+        <Routes>
+          <Route path='/' element={<Home/>}  />
+          <Route path='/addBlog' element={<AddEditBlog/>}  />
+          <Route path='/editBlog/:id' element={<AddEditBlog/>}  />
+          <Route path='/blog/:id' element={<Blog/>}  />
+          <Route path='/about' element={<About/>}  />
+          <Route path='*' element={<NotFound/>}  />
+
+        </Routes>
+      
+      </div>
+    </BrowserRouter>
+ 
+  );
+}
+
+export default App;
